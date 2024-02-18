2f1e337c4761f6e287b57b203e90f4e8bbb8b9b3
Mugrebot <mikeongay@gmail.com>
2024-02-17 19:18:52 -0500

    beep

--- src/app/api/echo/route.ts
+++ src/app/api/echo/route.ts
@@ -1,77 +1,57 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSSLHubRpcClient, Message } from "@farcaster/hub-nodejs";
 
-// Environment variables for HUB URL and the host URL.
-const HUB_URL = process.env.HUB_URL || "nemes.farcaster.xyz:2283";
-const HOST_URL = process.env.HOST || "https://your-deployment-url.com";
-
+const HUB_URL = process.env["HUB_URL"] || "nemes.farcaster.xyz:2283";
 const hubClient = getSSLHubRpcClient(HUB_URL);
 
-console.log("HUB_URL", HUB_URL);
+const postUrl = `${process.env["HOST"]}/api/code`;
 
 export async function POST(req: NextRequest) {
-  try {
-    // Parse the JSON body from the request
-    const { untrustedData, trustedData } = await req.json();
+  const {
+    untrustedData: { inputText },
+    trustedData: { messageBytes },
+  } = await req.json();
+  const frameMessage = Message.decode(Buffer.from(messageBytes, "hex"));
+  const validateResult = await hubClient.validateMessage(frameMessage);
+  if (validateResult.isOk() && validateResult.value.valid) {
+    const validMessage = validateResult.value.message;
 
-    console.log("beans");
-
-    // Decode the messageBytes from trustedData
-    const frameMessage = Message.decode(Buffer.from(trustedData.messageBytes, "hex"));
-    const validateResult = await hubClient.validateMessage(frameMessage);
-
-    if (validateResult.isOk() && validateResult.value.valid) {
-      const validMessage = validateResult.value.message;
-
-      // Ensure the URL in the frameActionBody starts with the HOST URL
-      let urlBuffer = validMessage?.data?.frameActionBody?.url ?? [];
-      const urlString = Buffer.from(urlBuffer).toString("utf-8");
-      console.log("urlString", urlString);
-      if (!urlString.startsWith(HOST_URL)) {
-        console.log("Invalid URL in frameActionBody", urlString);
-        return new NextResponse("Bad Request", { status: 400 });
-      }
-
-      // Construct the image URL for the social sharing unfurl
-      const message = untrustedData.inputText ?? "No message provided";
-      const imageUrl = `${HOST_URL}/api/images/echo?date=${Date.now()}&message=${encodeURIComponent(message)}`;
-
-      // Construct HTML response with appropriate metadata for social sharing
-      return new NextResponse(
-        `<!DOCTYPE html>
-        <html>
-          <head>
-            <title>Echo Says: ${message}</title>
-            <meta property="og:title" content="Echo Says: ${message}" />
-            <meta property="og:image" content="${imageUrl}" />
-            <meta name="fc:frame" content="vNext" />
-            <meta name="fc:frame:post_url" content="${HOST_URL}" />
-            <meta name="fc:frame:image" content="${imageUrl}" />
-            <meta name="fc:frame:button:1" content="See code" />
-            <meta name="fc:frame:button:1:action" content="post_redirect" />
-            <meta name="fc:frame:button:2" content="(coming soon)" />
-            <meta name="fc:frame:button:2:action" content="link" />
-            <meta name="fc:frame:button:2:target" content="https://github.com/horsefacts/echo-the-dolphin" />
-          </head>
-          <body/>
-        </html>`,
-        {
-          status: 200,
-          headers: {
-            "Content-Type": "text/html",
-          },
-        }
-      );
-    } else {
-      // If the message validation fails, return an Unauthorized response
-      return new NextResponse("Unauthorized", { status: 401 });
+    let urlBuffer = validMessage?.data?.frameActionBody?.url ?? [];
+    const urlString = Buffer.from(urlBuffer).toString("utf-8");
+    if (!urlString.startsWith(process.env["HOST"] ?? "")) {
+      return new NextResponse("Bad Request", { status: 400 });
     }
-  } catch (error) {
-    console.error('Error processing request:', error,);
-    // Return a Bad Request response in case of any errors during processing
-    return new NextResponse("Bad Request", { status: 400 });
+
+    const message = inputText ?? "";
+    const imageUrl = `${process.env["HOST"]}/api/images/echo?date=${Date.now()}&message=${message}`;
+    return new NextResponse(
+      `<!DOCTYPE html>
+      <html>
+        <head>
+          <title>Echo Says:</title>
+          <meta property="og:title" content="Echo Says:" />
+          <meta property="og:image" content="${imageUrl}" />
+          <meta name="fc:frame" content="vNext" />
+          <meta name="fc:frame:post_url" content="${postUrl}" />
+          <meta name="fc:frame:image" content="${imageUrl}" />
+          <meta name="fc:frame:button:1" content="See code" />
+          <meta name="fc:frame:button:1:action" content="post_redirect" />
+          <meta name="fc:frame:button:2" content="(coming soon)" />
+          <meta name="fc:frame:button:2:action" content="link" />
+          <meta name="fc:frame:button:2:target" content="https://github.com/horsefacts/echo-the-dolphin" />
+        </head>
+        <body/>
+      </html>`,
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "text/html",
+        },
+      }
+    );
+  } else {
+    return new NextResponse("Unauthorized", { status: 401 });
   }
 }
 
-// Allow GET requests to be handled the same way as POST for simplicity
 export const GET = POST;
